988fced85e23a1ebea7b87f2a428cec6be078922
Muhammad Quthbul Alam <100410118+MuhammadQuthbulAlam@users.noreply.github.com>
2024-07-27 23:33:50 +0700

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ window.onscroll = () => {
         let top = window.scrollY;
         let offset = sec.offsetTop - 150;
         let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
+        let id = sec.getAttribute('id');    
 
         if(top >= offset && top < offset + height){
             navLinks.forEach(links => {
